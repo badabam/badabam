430c54e9ff6878a227194c07ff8cdb9fc3c0469c
Jeremias Erbs <jerry@badabam.de>
2016-03-19 18:35:11 +0100

    Add intro animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,15 @@
 (function () {
   'use strict';
 
+  var $ = window.$;
   // fix safari label bug with empty function
   document.querySelectorAll('label').onClick = function () { return false; };
+
+  // show intro
+  $('.intro').each( (index, el) => {
+    $(el)
+      .addClass('intro-show')
+      .css({transitionDelay: index * 1 + 0.5 + 's'});
+  });
+
 }());
