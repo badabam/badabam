1c595b714108148b1224610a3001fd3014102f9e
Jeremias Erbs <jerry@badabam.de>
2019-05-13 18:27:58 +0200

    Add color changing

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,22 @@ document.querySelectorAll('label').onClick = function() {
   return false
 }
 
-// [primary, dark]
-const schemes = [['#FF5600', '#3e6a80'], ['#e94664', '#3f3a4d']]
+setTimeout(update, 5000)
+function update() {
+  const i = Math.round(Math.random() * 360)
 
-const randomIndex = Math.floor(Math.random() * schemes.length - 1) + 1
-const randomScheme = schemes[randomIndex]
-console.log(randomScheme)
+  const hue1 = Math.random() * 60
+  const hue2 = 100 - hue1
 
-const [primary, dark] = randomScheme
-const root = document.documentElement
-root.style.setProperty('--primary', primary)
-root.style.setProperty('--dark', dark)
+  const [firstHue, secondHue] =
+    Math.random() < 0.2 ? [hue1, hue2] : [hue2, hue1]
+
+  const colors = [
+    `hsl(${i}deg, ${firstHue}%, 60%)`,
+    `hsl(${i + 120}deg, ${secondHue}%, 30%)`,
+  ]
+
+  document.body.style.setProperty('--primary', colors[0])
+  document.body.style.setProperty('--secondary', colors[1])
+  setTimeout(update, 5000)
+}
